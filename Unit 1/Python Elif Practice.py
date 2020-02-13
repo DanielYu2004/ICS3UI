85ebaf7e188001f8ba42533dab37767d3c1c515d
Daniel Yu <danielyu2004@icloud.com>
2020-02-13 14:23:00 -0500

    Create Python Elif Practice.py

---
+++ Unit 1/Python Elif Practice.py
@@ -0,0 +1,14 @@
+steak = input().split("do you like steak? ")
+
+if steak == "yes":
+    cookness = input().split("do you like it rare, medium rare, medium, well done? ")
+    if cookness == "rare":
+        print("you are disgusting")
+    elif cookness == "medium rare":
+        print("you are a sane human being")
+    elif cookness == "medium":
+        print("you are not a garbage human being")
+    elif cookness == "welldone":
+        print("you are a garbage human being")
+else:
+    print("you should try a steak.")
