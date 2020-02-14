c85fe2e5a05c4969a4ad2d822563ee77d4304a7d
Daniel Yu <danielyu2004@icloud.com>
2020-02-14 12:42:45 -0500

    push

---
+++ Unit 2/Practice Exercises #2-2.py
@@ -0,0 +1,49 @@
+"""
+grade = int(input("Enter your grade: "))
+
+if grade > 12 or grade < 1:
+    print("I don’t know what school level you’re in.") 
+elif grade >= 9:
+    print("Highschool")
+elif grade >=7:
+    print("Middle School")
+elif grade >= 1:
+    print("Elementary School")
+"""
+"""
+light = input("Enter the trafficlight: ")
+
+if light == 'red':
+    print("stop")
+    print("wait until green")
+    print("then go")
+elif light == 'yellow':
+    distance = int(input("how far away is the intersection in meters"))
+    if distance > 20:
+        print("stop")
+        print("wait for green")
+        print("then go")
+    else:
+        print("go")
+elif light == "green":
+    print("go")
+else:
+    print("pull over and make an oppointment with the optometrist to get your eyes checked")
+"""
+"""
+numCredits = int(input('Enter the number of credits you have earned: '))
+
+if numCredits > 30:
+    litTest = input("have you passed the literacy test? ")
+    if litTest == "yes":
+        hours = int(input("How many volunteer hours have you earned? "))
+        if hours > 40:
+            print("You can graduate")
+        else:
+            print("you need more hours to graduate")
+    else:
+        print("you need to pass the literacy test")
+else:
+    print("earn more credits")
+"""
+
