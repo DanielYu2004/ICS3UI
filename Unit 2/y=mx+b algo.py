b303a536c527d3b174575126e65d2d59eaf5c770
Daniel Yu <danielyu2004@icloud.com>
2020-02-18 12:50:06 -0500

    Create y=mx+b algo.py

---
+++ Unit 2/y=mx+b algo.py
@@ -0,0 +1,20 @@
+m = int(input("Enter the slope: "))
+b = int(input("Enter the y-intercept: "))
+
+if b > 0:
+    bOutput = "+" + str(b)
+elif b< 0:
+    bOutput = str(b)
+else:
+    bOutput = ""
+
+if m == 0:
+    mOutput = ""
+    bOutput = str(b)
+elif m == 1:
+    mOutput = "x"
+elif m == -1:
+    mOutput = "-x"
+else: 
+    mOutput = str(m) + "x"
+print("The equation of the line is y = " + mOutput + bOutput )
