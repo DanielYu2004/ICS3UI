22e81f0a12d378b3322c1ea47f26fff0067adb83
Daniel Yu <danielyu2004@icloud.com>
2020-02-19 14:00:10 -0500

    d

---
+++ Unit 2/Assignment 2/Fraction Reducer.py
@@ -0,0 +1,34 @@
+print("Welcome to Daniel's Fraction Reducer")
+print("***********************************")
+
+def EuclideanAlgo(bigger, smaller):    
+    if bigger % smaller == 0:
+        return(smaller)
+    
+    elif bigger > 0 and smaller > 0:
+        return(EuclideanAlgo(smaller, bigger % smaller))
+
+while True:
+    print()
+    numerator = int(input("Please enter the numerator of the fraction: "))
+    denominator  = int(input("Please enter the denominator  of the fraction: "))
+
+    if denominator == 0:
+        print("This fraction is undefined.")
+    elif numerator == 0:
+        print(str(numerator) + "/" + str(denominator) ,"=","0")
+    elif numerator < 0:
+        print("Sorry, this program only handles non-negative numerators.")
+    elif denominator < 0:
+        print("Sorry, this program only handles non-negative denominator.")
+    else:
+        GCD = (EuclideanAlgo(max(numerator, denominator), min(numerator, denominator)))
+        if denominator / GCD == 1:
+            print("Here is your reduced fraction:")
+            print(str(numerator) + "/" + str(denominator) ,"=",str(int(numerator/GCD)))
+            print()
+        elif GCD == 1:
+            print(str(int(numerator/GCD)) +"/" + str(int(denominator/GCD)) , "is already in reduced form")
+        else:
+            print("Here is your reduced fraction:")
+            print(str(numerator) + "/" + str(denominator) ,"=",str(int(numerator/GCD)) +"/" + str(int(denominator/GCD)))
