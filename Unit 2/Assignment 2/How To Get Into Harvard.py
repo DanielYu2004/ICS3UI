c03b631149334f0e7c5dadd1304df13eb4449f63
Daniel Yu <danielyu2004@icloud.com>
2020-02-20 10:11:00 -0500

    Update How To Get Into Harvard.py

--- Unit 2/Assignment 2/How To Get Into Harvard.py
+++ Unit 2/Assignment 2/How To Get Into Harvard.py
@@ -1,6 +1,6 @@
 import sys
 
-print("This is a Harvard issued program that can help you determine your admission chances into the coveted Ivy League School... HARVARD")
+print("This is a Harvard issued program that can help you determine your admission chances into the coveted Ivy League School...HARVARD")
 print("We use a highly accurate algorithm to calculate your probability given many different qualitative and qualitative factors")
 print()
 
@@ -14,49 +14,60 @@ if res1 == "no" or res1 == "No":
     print("Well then why are you on this program then...")
 elif res1 == "yes" or res1 == "Yes":
     print()
-    print("Very nice answer. Alright next question...")
+    print("Very nice answer. Alright next question... Question 2:")
     average = float(input("What is your cumulative average out of 100? "))
     if average < 90:
+        print()
         print("Sorry, you were almost there buddy. Goodbye.")
     elif average > 100:
+        print()
         print("I don't believe you...")
     else:
         if average < 95:
+            print()
             print("Hmmm, not bad")
             score +=10
         elif average < 100:
+            print()
             print("Hey, thats pretty good")
             score += 20
         elif average == 100:
+            print()
             print("Thats very very nice bud.")
             score += 30
-
-        print("Just a few more questions...")
+        print()
+        print("Just a few more questions... Question 3:")
         print()
 
         height = int(input("Please enter your height in centimeters: "))
 
         if height < 160:
+            print()
             print("Awwww, just a little taller and you could've made it...")
         elif height > 200:
+            print()
             print("You might have some trouble going through doors. Maybe next time...")
         else:
             if height < 170:
+                print()
                 print("hmm, thats an interesting height for a student")
                 score += 10
             elif height < 180:
+                print()
                 print("you can comfortably walk through doors. Nice.")
                 score += 20
             elif height < 190:
+                print()
                 print("you might hit your head on the door, but you'll be fine.")
                 score += 30
             elif height <= 200:
+                print()
                 print("I would watch your head when you walk through doors. Just don't hit your head too hard.")
                 score +=20
-            
-            print("Alright, maybe like 2 more questions... maybe...")
             print()
-            SteakCook = input("How do you like your steak cooked... Choose from [raw, rare, medium rare, medium, well done, burnt to a crisp]")
+            print("Alright, maybe like 2 more questions... maybe... Question 4:")
+            print()
+            SteakCook = input("How do you like your steak cooked... Choose from: raw, rare, medium rare, medium, well done, burnt to a crisp...")
             SteakOptions = ['raw', 'rare', 'medium rare', 'medium', 'well done', 'burnt to a crisp']
 
             if SteakCook not in SteakOptions:
