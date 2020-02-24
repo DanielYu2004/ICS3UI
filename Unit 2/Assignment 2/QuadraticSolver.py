7a27f761a4b0f721893104e0a6ae581b1ba08a39
Daniel Yu <danielyu2004@icloud.com>
2020-02-24 14:25:20 -0500

    d

--- Unit 2/Assignment 2/QuadraticSolver.py
+++ Unit 2/Assignment 2/QuadraticSolver.py
@@ -14,64 +14,95 @@ if power > 2:
     print("Sorry, this program cannot handle polynomials of degree 3 or higher")
 elif power == 1:
     #Handling polynomial of degree 1
+    print("degree1")
     pass
 elif power == 2:
     #Handling polynomial of degree 2
     quadratic = input("Please enter your quadratic in the form ax^2+bx+c=0 (eg: -5x^2-4x+3=0): ")
 
-    #Variable to keep track of index of the quadratic 
-    index = 0
-
-    #Variable for the coefficient of x^2
-    a = ""
-
-    #Loop to parce for coefficient of x^2
-    while quadratic[index] != "x":
-        a += quadratic[index]
-        index += 1
-
-    #Move index to next index after x^2
-    index += 3
-
-    #Variable for the coefficient of x
-    b = ""
-
-    #Loop to parce for coefficient of x
-    while quadratic[index] != "x":
-        b += quadratic[index]
-        index += 1
-    #Move index to next index for x
-    index += 1
-
-    #Variable for the constant
-    c = ""
-
-    #Loop to parce for constant
-    while quadratic[index] != "=" and index < len(quadratic):
-        c += quadratic[index]
-        index += 1
-
-    a = float(a)
-    b = float(b)
-    c = float(c)
-
-    if a == 0:
-        print("I guess your quadratic was not to degree 2")
+    if "x^2" not in quadratic:
+        print("You didn't put a quadratic in degreee 2")
     else:
-        sol1 = (-b + sqrt((b**2)-(4*a*c)))/2
-        sol2 = (-b - sqrt((b**2)-(4*a*c)))/2
-        print(sol1)
-        print(sol2)
-    print(a)
-    print(b)
-    print(c)
+        #Variable to keep track of index of the quadratic 
+        index = 0
 
-    
+        #Variable for the coefficient of x^2
+        a = ""
 
-    
+        #Loop to parce for coefficient of x^2
+        while quadratic[index] != "x":
+            a += quadratic[index]
+            index += 1
+
+        #Move index to next index after x^2
+        index += 3
+
+        #Variable for the coefficient of x
+        b = ""
+        nextvar = True
+        #Loop to parce for coefficient of x
+        while quadratic[index] != "x":
+
+            #if this condition is satisfied, that means the coefficient of "x" is 0
+            if quadratic[index] == "=":
+                c = b
+                b = 0
+                #print(c)
+
+                nextvar = False
+                break
+            b += quadratic[index]
+            index += 1
+        #Move index to next index for x
+        index += 1
+
+        #Condition is met when the coefficient of "x" is not 0
+        if nextvar == True:
+            #print(c)
+            #Variable for the constant
+            c = ""
+        #Loop to parce for constant
+            while quadratic[index] != "=" and index < len(quadratic):
+                c += quadratic[index]
+                index += 1
+            if c == "":
+                c = 0
+
+
+        if a == "":
+            a = 1
+        if b == "":
+            b = 1
+        if c == "":
+            c = 1
+
+        a = float(a)
+        b = float(b)
+        c = float(c)
+
+        if a == 0:
+            print("I guess your quadratic was not to degree 2")
+        else:
+            discriminant = (b**2) - (4*a*c)
+            if discriminant >= 0:
+                sol1 = ((-1*b) +  sqrt((b**2) - (4*a*c)))/(2*a)
+                sol2 = (-b - sqrt((b**2)-(4*a*c)))/(2*a)
+                print("The solutions to the quadratic are:" , sol1 , "and" , sol2)
+            else:
+                print("The solutions are complex")
+
+            #print(sol1)
+            #print(sol2)
+        #print(a)
+        #print(b)
+        #print(c)
+
+        
+
+        
 
 
 
-    #print(quadratic)
+        #print(quadratic)
 
 
