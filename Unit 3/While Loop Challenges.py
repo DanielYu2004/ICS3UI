1e78b4a32f8531996af8ed4374b58b8552737c27
Daniel Yu <danielyu2004@icloud.com>
2020-02-25 13:45:58 -0500

    d

---
+++ Unit 3/While Loop Challenges.py
@@ -0,0 +1,9 @@
+answer = 200
+
+userAns = int(input("What is 40000 square root? "))
+
+while userAns != answer:
+    print("Nope.")
+    userAns = int(input("Please try again:"))
+print("Nice Job")
+ 
