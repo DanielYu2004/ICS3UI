39235c0a4686805b3e599a6491094656887fa912
DanielYu2004 <DanielYu2004@users.noreply.github.com>
2020-04-30 23:36:22 -0400

    Update 2020 Summer Olympics.py

--- Unit 4/2020 Summer Olympics.py
+++ Unit 4/2020 Summer Olympics.py
@@ -4,35 +4,109 @@ import math
 import random
 myInterface = Tk()
 
+
+
+
 screen = Canvas(myInterface, width=1000, height=700, background="sky blue")
 screen.pack()
+
+
+
+# #Grid lines
+# spacing = 50
+
+# for x in range(0, 1000, spacing): 
+#     screen.create_line(x, 25, x, 1000, fill="blue")
+#     screen.create_text(x, 5, text=str(x), font="Times 9", anchor = N)
+
+# for y in range(0, 1000, spacing):
+#     screen.create_line(25, y, 1000, y, fill="blue")
+#     screen.create_text(5, y, text=str(y), font="Times 9", anchor = W)
+
+# screen.update()
+
 
 # Foreground
 screen.create_rectangle(0, 700, 1000, 600, fill="green")
 
 # Track
-screen.create_rectangle(0, 600, 1000, 300, fill="grey", outline="grey")
+screen.create_rectangle(0, 600, 1000, 300, fill="#141414", outline="#141414")
 
 buildings = []
-
 cloudList = []
+roadLines = []
 
-def createBuilding(coords):
+cameraSpeed = 5
+lineWidth = 50
+lineHeight = 15
+
+
+def createBuilding(coords, colour):
     x1 = coords[0]
     y1 = coords[1]
     x2 = coords[2]
     y2 = coords[3]
     buildingComponents = []
-    buildingComponents.append(screen.create_rectangle(x1, y1, x2, y2, fill="red"))
+    buildingComponents.append(screen.create_rectangle(x1, y1, x2, y2, fill=colour))
     #buildingComponents.append(screen.create_rectangle(x1+50, y1+50, x2-50, y2-50, fill="red"))
     return buildingComponents
 
+def createLine(coords):
+    x1 = coords[0]
+    y1 = coords[1]
+    x2 = x1 + lineWidth
+    y2 = y1 + lineHeight
+    lineCoords = []
+    lineCoords.append(screen.create_rectangle(x1, y1, x2, y2, fill="white"))
+    return lineCoords
 
-buildings.append([800, 300, 1000, 200])
+def drawRunner(coords, frame):
+    x1 = 500
+    y1 = 350
+    skinColour = "#ffd17a"
+    if frame == 1:
+        screen.create_polygon(x1, y1 + 10, x1 - 10, y1 + 30, x1 - 20, y1 + 35, x1 - 25, y1 + 35, fill=skinColour)
 
+        screen.create_rectangle(x1-8, y1-10, x1+8, y1+20, fill="red")
+        screen.create_oval(x1 - 8, y1-10, x1 + 8, y1 - 10 - 16, fill=skinColour)
+
+        screen.create_polygon(x1, y1 + 10, x1 +10, y1 + 10 + 5, x1 + 15, y1 + 10 + 8, x1 + 20, y1 + 10 + 30, x1 + 15, y1 + 10 + 30, x1 + 15, y1 + 10 + 20, x1, y1 + 15,fill=skinColour)
+    if frame == 2:
+        x1 = 600
+        y1 = 350
+        screen.create_rectangle(x1-8, y1-10, x1+8, y1+20, fill="red")
+        screen.create_oval(x1 - 8, y1-10, x1 + 8, y1 - 10 - 16, fill=skinColour)
+
+    if frame == 3:
+        x1 = 700
+        y1 = 350
+        screen.create_polygon(x1, y1 + 10, x1 +10, y1 + 10 + 5, x1 + 15, y1 + 10 + 8, x1 + 20, y1 + 10 + 30, x1 + 15, y1 + 10 + 30, x1 + 15, y1 + 10 + 20, x1, y1 + 15,fill=skinColour)
+
+        screen.create_rectangle(x1-8, y1-10, x1+8, y1+20, fill="red")
+        screen.create_oval(x1 - 8, y1-10, x1 + 8, y1 - 10 - 16, fill=skinColour)
+
+        screen.create_polygon(x1, y1 + 10, x1 - 10, y1 + 30, x1 - 20, y1 + 35, x1 - 25, y1 + 35, fill=skinColour)
+
+
+
+
+for i in range(4):
+    colour = random.choice(["#a8a8a8", "#858585", "#787474"])
+    x1 = random.randint(0,1000)
+    height = random.randint(200, 400)
+    buildings.append([[x1, 300, x1+random.randint(100, 300), 300 - height], colour])
+
+increment = (1000-(8*lineWidth))/7
+for i in range(1,9):
+    x1 = (increment+lineWidth)*(i-1) 
+    y1 = 450-(lineHeight/2)
+    roadLines.append([x1,y1])
+
+drawRunner(1,1)
+drawRunner(1,2)
+drawRunner(1,3)
 
 for f in range(1000000):
-
 
     # For Clouds
     if f % 40 == 0:
@@ -54,7 +128,7 @@ for f in range(1000000):
                     if bigC[littleC][coord] < -100:
                         appendBool = False
                     if coord % 2 == 0:
-                        newCoords.append(bigC[littleC][coord] - 5)
+                        newCoords.append(bigC[littleC][coord] - cameraSpeed)
                     else:
                         newCoords.append(bigC[littleC][coord])
                 bigC[littleC] = newCoords
@@ -63,32 +137,45 @@ for f in range(1000000):
     
         
 
-
+    # For Buildings
     if f % 60 == 0:
-        
-        buildings.append([1000, 300, 1000 + random.randint(100, 300), 300 - random.randint(150,250)])
+        colour = random.choice(["#a8a8a8", "#858585", "#787474"])
+        buildings.append([[1000, 300, 1000 + random.randint(100, 400), 300 - random.randint(100,400)], colour])
 
     renderBuildings = []
     updatedBuildings = []
     for building in range(len(buildings)):
-        if buildings[building]:
-            print(buildings)
-            print(buildings[building])
-            x2 = buildings[building][2]
+        if buildings[building][0]:
+            x2 = buildings[building][0][2]
             if x2 > 0:
-                coords = buildings[building]
-                coords[0] -= 5
-                coords[2] -= 5
-                renderBuildings.append(createBuilding(coords))
+                coords = buildings[building][0]
+                colour = buildings[building][1]
+                coords[0] -= cameraSpeed
+                coords[2] -= cameraSpeed
+                renderBuildings.append(createBuilding(coords, colour))
                 updatedBuildings.append(buildings[building])
-            # else:
-            #     updatedBuildings[building] = []
     buildings = updatedBuildings
 
-            
-    print(buildings)
+    # For Lines
+
+    renderLines = []
+    updatedLines = []
+    for line in roadLines:
+        if line:
+            if line[0]+lineWidth > 0:
+                line[0] = line[0] - cameraSpeed
+                renderLines.append(createLine(line))
+                updatedLines.append(line)
+            else:
+                line[0] = 1000
+                renderLines.append(createLine(line))
+                updatedLines.append(line)
+    print(renderLines)
+    roadLines = updatedLines
+
+
     
-    
+ 
     
     
     
@@ -121,6 +208,9 @@ for f in range(1000000):
     
     for building in renderBuildings:
         screen.delete(building)
+        
+    for line in renderLines:
+        screen.delete(line)
     
     
     
